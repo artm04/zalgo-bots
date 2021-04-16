52b13b3470f554bfadb29d88d88587ea66db667d
DeMax <trollbol213@gmail.com>
2021-04-16 22:09:33 +0300

    feat: now logging works too

--- user_interfaces.py
+++ user_interfaces.py
@@ -29,7 +29,7 @@ class DiscordInterface(UserInterface):
 
         @self.client.event
         async def on_ready():
-            print('We have logged in as {0.user}'.format(self.client))
+            self.logger.log_to_console_and_file('We have logged in as {0.user}'.format(self.client))
 
         @self.client.event
         async def on_message(message):
@@ -37,12 +37,14 @@ class DiscordInterface(UserInterface):
                 return
 
             if message.content.startswith('/zalgo'):
+                self.logger.log_to_console_and_file("Discord: /zalgo")
                 if message.content == "/zalgo":
                     await message.channel.send("Write text to be zalgofied after the /zalgo")
                 else:
                     await message.channel.send(self.zalgo_text.zalgofy(message.content[6:]))
 
             elif "private" in message.channel.type:  # DM. message.channel.type == "private" doesn't work
+                self.logger.log_to_console_and_file("Discord: PM's")
                 await message.channel.send(self.zalgo_text.zalgofy(message.content))
 
     def run(self):
@@ -57,6 +59,7 @@ class TelegramInterface(UserInterface):
 
         @self.client.message_handler(commands=['zalgo'])
         def zalgofy_command(message: telebot.types.Message):
+            self.logger.log_to_console_and_file("Telegram: /zalgo")
             if message.text == "/zalgo":
                 self.client.reply_to(message, "Write text to be zalgofied after the /zalgo")
             else:
@@ -65,10 +68,12 @@ class TelegramInterface(UserInterface):
 
         @self.client.message_handler(func=lambda message: message.chat.type == 'private')
         def zalgofy_pm(message: telebot.types.Message):
+            self.logger.log_to_console_and_file("Telegram: PM's")
             self.client.reply_to(message, self.zalgo_text.zalgofy(message.text))
 
         @self.client.inline_handler(func=lambda query: len(query.query) > 0)
         def zalgofy_inline(query: telebot.types.InlineQuery):
+            self.logger.log_to_console_and_file("Telegram: Inline")
             zalgofied_text = self.zalgo_text.zalgofy(query.query)
             answer = telebot.types.InlineQueryResultArticle('1', zalgofied_text,
                                                             telebot.types.InputTextMessageContent(zalgofied_text))
