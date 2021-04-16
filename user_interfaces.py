2bf2f723023e79238c7f1474166f4a3589bd10cf
Artem <artem.skripnik3004@gmail.com>
2021-04-16 14:53:38 +0300

    feat: implement TelegramInterface

--- user_interfaces.py
+++ user_interfaces.py
@@ -1,6 +1,7 @@
 from threading import Thread
 from abc import ABC, abstractmethod
 import discord
+import telebot
 from main import ZalgoText
 
 zalgo = ZalgoText()
@@ -45,3 +46,29 @@ class DiscordInterface(UserInterface):
 
     def run(self):
         self.client.run(self.token)
+
+
+class TelegramInterface(UserInterface):
+
+    def __init__(self, logger, zalgo_text, token: str):
+        super().__init__(logger, zalgo_text, token)
+        self.client = telebot.TeleBot(token)
+
+        @self.client.message_handler(commands=['zalgofy'])
+        def zalgofy_command(message: telebot.types.Message):
+            source = telebot.util.extract_arguments(message.text)
+            self.client.reply_to(message, self.zalgo_text.zalgofy(source))
+
+        @self.client.message_handler(func=lambda message: message.chat.type == 'private')
+        def zalgofy_pm(message: telebot.types.Message):
+            self.client.reply_to(message, self.zalgo_text.zalgofy(message.text))
+
+        @self.client.inline_handler(func=lambda query: len(query.query) > 0)
+        def zalgofy_inline(query: telebot.types.InlineQuery):
+            zalgofied_text = self.zalgo_text.zalgofy(query.query)
+            answer = telebot.types.InlineQueryResultArticle('1', zalgofied_text,
+                                                            telebot.types.InputTextMessageContent(zalgofied_text))
+            self.client.answer_inline_query(query.id, [answer])
+
+    def run(self):
+        self.client.infinity_polling()
